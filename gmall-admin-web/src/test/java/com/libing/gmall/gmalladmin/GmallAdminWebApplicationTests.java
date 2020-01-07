a011e67f5e36eae7b6f0ee78ecc71af7f33d4b2a
lib-dev <851137275@qq.com>
2020-01-07 20:14:37 +0800

    项目结构创建完成

---
+++ gmall-admin-web/src/test/java/com/libing/gmall/gmalladmin/GmallAdminWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.libing.gmall.gmalladmin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GmallAdminWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
